6d3b0bc15735236defa94b01d7f500efd0fbd84e
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-03-14 21:25:07 +0000

    refactored with BigDecimal

--- CashRegister.java
+++ CashRegister.java
@@ -1,35 +1,39 @@
+import java.math.*;
 
 /**
    A cash register totals up sales and computes change due.
 */
 public class CashRegister
 {
-   private double purchase;
-   private double payment;
+   private BigDecimal purchase;
+   private BigDecimal payment;
+
    /**
       Constructs a cash register with no money in it.
    */
    public CashRegister()
    {
-      purchase = 0;
-      payment = 0;
+      purchase = new BigDecimal ("0.00");
+      payment = new BigDecimal ("0.00");
    }
 
    /**
       Records the sale of an item.
       @param amount the price of the item
    */
-   public void recordPurchase(double amount)
+   public void recordPurchase(BigDecimal amount)
    {
-      double total = purchase + amount;
+      //BigDecimal total = purchase + amount;
+      BigDecimal total = purchase.add(amount);
       purchase = total;
+      System.out.println("*** Record purchase: "+purchase);
    }
 
    /**
       Enters the payment received from the customer.
       @param amount the amount of the payment
    */
-   public void enterPayment(double amount)
+   public void enterPayment(BigDecimal amount)
    {
       payment = amount;
    }
@@ -38,9 +42,10 @@ public class CashRegister
       Computes the change due
       @return the change due to the customer
    */
-   public double calculateChange()
+   public BigDecimal calculateChange()
    {   
-      double change = payment - purchase;
+      //BigDecimal change = payment - purchase;
+      BigDecimal change = payment.subtract(purchase);
       //purchase = 0;
       //payment = 0;
       return change;
@@ -51,17 +56,19 @@ public class CashRegister
 */
    public void giveChange()
    {
-      double change = calculateChange();
-      int pounds = 0;
-      double pennies = 0.0;
+      BigDecimal change = calculateChange();
+      BigDecimal pounds = new BigDecimal ("0.00");
+      BigDecimal pennies = new BigDecimal ("0.00");
 
-      double currentBalance = change;
+      BigDecimal currentBalance = change;
+      System.out.println("*** Current balance: "+currentBalance);
 
-      if (currentBalance > 1.0) {
-         pounds = (int) currentBalance;
-         pennies = currentBalance - pounds;
+      //if (currentBalance > 1.0) {
+      if (currentBalance.compareTo(new BigDecimal("1.00")) > 0) {
+         pounds = currentBalance.setScale(0, RoundingMode.FLOOR);
+         pennies = currentBalance.subtract(pounds);
          System.out.println("£1 coins: "+pounds);
-         currentBalance -= pounds;
+         currentBalance = currentBalance.subtract(pounds);
       }
 
       int fiftyPence = 0;
@@ -71,50 +78,57 @@ public class CashRegister
       int twoPence = 0;
       int onePence = 0;
 
-      if (currentBalance > 0.49) {
+      //if (currentBalance > 0.49) {
+      if (currentBalance.compareTo(new BigDecimal("0.49")) > 0) {
          fiftyPence += 1;
          System.out.println("50p coins: "+fiftyPence);
-         currentBalance -= 0.5;
+         currentBalance = currentBalance.subtract(new BigDecimal("0.5"));
       }
-      if (currentBalance > 0.39) {
+      //if (currentBalance > 0.39) {
+      if (currentBalance.compareTo(new BigDecimal("0.39")) > 0) {
          twentyPence += 2;
          System.out.println("20p coins: "+twentyPence);
-         currentBalance -= 0.4;
+         currentBalance = currentBalance.subtract(new BigDecimal("0.4"));
       }
-      if (currentBalance > 0.19) {
+      //if (currentBalance > 0.19) {
+      if (currentBalance.compareTo(new BigDecimal("0.19")) > 0) {
          twentyPence += 1;
          System.out.println("20p coins: "+twentyPence);
-         currentBalance -= 0.2;
+         currentBalance = currentBalance.subtract(new BigDecimal("0.2"));
       }
-      if (currentBalance > 0.09) {
+      //if (currentBalance > 0.09) {
+      if (currentBalance.compareTo(new BigDecimal("0.09")) > 0) {
          tenPence += 1;
          System.out.println("10p coins: "+tenPence);
-         currentBalance -= 0.1;
+         currentBalance = currentBalance.subtract(new BigDecimal("0.1"));
       }
-      if (currentBalance > 0.049) {
+      //if (currentBalance > 0.049) {
+      if (currentBalance.compareTo(new BigDecimal("0.049")) > 0) {
          fivePence += 1;
          System.out.println("5p coins: "+fivePence);
-         currentBalance -= 0.05;
+         currentBalance = currentBalance.subtract(new BigDecimal("0.05"));
       }
-      if (currentBalance > 0.039) {
+      //if (currentBalance > 0.039) {
+      if (currentBalance.compareTo(new BigDecimal("0.039")) > 0) {
          twoPence += 2;
          System.out.println("2p coins: "+twoPence);
-         currentBalance -= 0.04;
+         currentBalance = currentBalance.subtract(new BigDecimal("0.04"));
       }
-      if (currentBalance > 0.019) {
+      //if (currentBalance > 0.019) {
+      if (currentBalance.compareTo(new BigDecimal("0.019")) > 0) {
          twoPence += 1;
          System.out.println("2p coins: "+twoPence);
-         currentBalance -= 0.02;
+         currentBalance = currentBalance.subtract(new BigDecimal("0.02"));
       }
-      if (currentBalance > 0.009) {
+      //if (currentBalance > 0.009) {
+      if (currentBalance.compareTo(new BigDecimal("0.009")) > 0) {
          onePence += 1;
          System.out.println("1p coins: "+onePence);
-         currentBalance -= 0.01;
+         currentBalance = currentBalance.subtract(new BigDecimal("0.01"));
       }
       
-      purchase = 0;
-      payment = 0;
+      purchase = new BigDecimal ("0.00");
+      payment = new BigDecimal ("0.00");
 
    }
-
 }
--- CashRegisterTest.java
+++ CashRegisterTest.java
@@ -1,3 +1,5 @@
+import java.math.BigDecimal;
+
 /**
    A class to test the CashRegister class.
 */
@@ -6,10 +8,10 @@ public class CashRegisterTest
    public static void main(String[] args)
    {
       CashRegister register = new CashRegister();
-      register.recordPurchase(13.50);
-      register.recordPurchase(2.25);
-      register.enterPayment(20);
-      double change = register.calculateChange();
+      register.recordPurchase(new BigDecimal("13.50"));
+      register.recordPurchase(new BigDecimal("2.25"));
+      register.enterPayment(new BigDecimal("20"));
+      BigDecimal change = register.calculateChange();
       System.out.println(change);
       register.giveChange();
    }
